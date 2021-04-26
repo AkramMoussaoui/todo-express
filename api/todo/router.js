6acb4699b8787eb2f92424c16cd398e5c4b1078b
Akram Moussaoui <akrammoussaoui14@gmail.com>
2021-04-26 13:46:21 +0100

    add todo validation

--- api/todo/router.js
+++ api/todo/router.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const controller = require("./controller");
-
+const schema = require("../../validators/todo");
+const validate = require("../../validators/validate");
 const todoRouter = express.Router();
 
 todoRouter.get("/", controller.getTodos);
 
 todoRouter.get("/:id", controller.getOneTodo);
 
-todoRouter.post("/", controller.addTodo);
+todoRouter.post("/", validate(schema.post), controller.addTodo);
 
-todoRouter.put("/:id", controller.updateTodo);
+todoRouter.put("/:id", validate(schema.put), controller.updateTodo);
 
 todoRouter.patch("/:id", controller.patchTodo);
 
