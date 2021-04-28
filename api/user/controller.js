6ccff93ae8411d373822ca73563be0635ddad0fb
Akram Moussaoui <akrammoussaoui14@gmail.com>
2021-04-28 13:33:47 +0100

    add models

--- api/user/controller.js
+++ api/user/controller.js
@@ -1,5 +1,5 @@
-const { v4: uuidv4 } = require("uuid");
-
+// const { v4: uuidv4 } = require("uuid");
+const User = require("../../models/user");
 let users = [
   {
     id: 1,
@@ -11,54 +11,89 @@ let users = [
   },
 ];
 
-const getUsers = (req, res) => {
+const getUsers = async (req, res) => {
+  const users = await User.find();
   res.status(200).send(users);
 };
-const addUser = (req, res) => {
+const addUser = async (req, res) => {
   const { nom, prenom, phone, email } = req.body;
-  const user = {
-    id: uuidv4(),
+  // const user = {
+  //   id: uuidv4(),
+  //   nom,
+  //   prenom,
+  //   email,
+  //   phone,
+  //   isActif: true,
+  // };
+  // users.push(user);
+  const user = new User({
     nom,
     prenom,
     email,
     phone,
     isActif: true,
-  };
-  users.push(user);
+  });
+
+  await user.save();
+
   res.status(201).send(user);
 };
-const updateUser = (req, res) => {
+const updateUser = async (req, res) => {
   const { id } = req.params;
   const { nom, prenom, email, phone } = req.body;
 
-  const user = users.find((elem) => elem.id == id);
+  //const user = users.find((elem) => elem.id == id);
+  const user = await User.findById(id);
+
   if (!user) {
     return res.status(400).send({
       message: "user not found",
     });
   }
 
-  user.nom = nom || user.nom;
-  user.prenom = prenom || user.prenom;
-  user.email = email || user.email;
-  user.phone = phone || user.phone;
+  // user.nom = nom || user.nom;
+  // user.prenom = prenom || user.prenom;
+  // user.email = email || user.email;
+  // user.phone = phone || user.phone;
 
-  res.status(200).send(user);
+  const user2 = {};
+  user2.nom = nom || user.nom;
+  user2.prenom = prenom || user.prenom;
+  user2.email = email || user.email;
+  user2.phone = phone || user.phone;
+
+  const updatedUser = await User.findByIdAndUpdate(id, user2, { new: true });
+
+  if (!updatedUser) {
+    return res.status(500).send({
+      message: "Server error",
+    });
+  }
+
+  res.status(200).send(updatedUser);
 };
-const deleteUser = (req, res) => {
+const deleteUser = async (req, res) => {
   const { id } = req.params;
-  const user = users.find((elem) => elem.id == id);
+  //const user = users.find((elem) => elem.id == id);
+  const user = await User.findById(id);
   if (!user) {
     return res.status(400).send({
       message: "user not found",
     });
   }
-  users = users.filter((elem) => elem.id != id);
-  res.status(200).send(user);
+  // users = users.filter((elem) => elem.id != id);
+  const removedUser = await User.findByIdAndDelete(id);
+  if (!removedUser) {
+    return res.status(500).send({
+      message: "Server error",
+    });
+  }
+  res.status(200).send(removedUser);
 };
-const getOneUser = (req, res) => {
+const getOneUser = async (req, res) => {
   const { id } = req.params;
-  const user = users.find((elem) => elem.id == id);
+  //const user = users.find((elem) => elem.id == id);
+  const user = await User.findById(id);
   if (!user) {
     return res.status(400).send({
       message: "user not found",
@@ -66,16 +101,29 @@ const getOneUser = (req, res) => {
   }
   res.status(200).send(user);
 };
-const patchUser = (req, res) => {
+const patchUser = async (req, res) => {
   const { id } = req.params;
-  const user = users.find((elem) => elem.id == id);
+  //const user = users.find((elem) => elem.id == id);
+  const user = await User.findById(id);
   if (!user) {
     return res.status(400).send({
       message: "user not found",
     });
   }
-  user.isActif = !user.isActif;
-  res.status(200).send(user);
+  // user.isActif = !user.isActif;
+  const patchedUser = await User.findByIdAndUpdate(
+    id,
+    {
+      isActif: !user.isActif,
+    },
+    { new: true }
+  );
+  if (!patchedUser) {
+    return res.status(500).send({
+      message: "Server error",
+    });
+  }
+  res.status(200).send(patchedUser);
 };
 
 module.exports = {
--- validators/validate.js
+++ validators/validate.js
@@ -1,3 +1,4 @@
+const Joi = require("Joi");
 const validate = (schema) => {
   return (req, res, next) => {
     const template = Joi.object(schema);
